3cc1801bfdef27304a25b25889a1c3361f78e883
I3ryI3e <jgrunho@gmail.com>
2020-09-16 17:22:58 +0100

    Front Page Done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQwC4Omogv90Goyh3Kz5XWGZxHDVkdjE4",
+    authDomain: "clone-d25f1.firebaseapp.com",
+    databaseURL: "https://clone-d25f1.firebaseio.com",
+    projectId: "clone-d25f1",
+    storageBucket: "clone-d25f1.appspot.com",
+    messagingSenderId: "898301001954",
+    appId: "1:898301001954:web:891d960a24ac08250f5ebb",
+    measurementId: "G-QFVM04X50X"
+};
